70bb1a7918c80f4f6bc7caef7cc66afb95bee6ad
Your Name <you@example.com>
2021-06-14 23:13:53 +0100

    server setup and port number changed

--- api/server.js
+++ api/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const session = require('express-session');
+const Store = require('connect-session-knex')(session);
+
+const authRouter = require('./auth/auth-router.js');
+const usersRouter = require('./users/users-router.js');
 
 /**
   Do what needs to be done to support sessions with the `express-session` package!
@@ -17,9 +22,31 @@ const cors = require("cors");
 
 const server = express();
 
+server.use(session({
+  name: 'foo',
+  secret: 'keep it secret', // .env file
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false, // if true, only works over TLS/https
+    httpOnly: false, // if true, cookie not in document
+  },
+  resave: false, // required by some session stores
+  saveUninitialized: false, // session not saved automatically
+  store: new Store({
+    knex: require('../data/db-config'),
+    tableName: 'sessions',
+    sidfieldname: 'sid',
+    createtable: true,
+    clearInterval: 1000 * 60 * 60,
+  })
+}));
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.use('/api/users', usersRouter);
+server.use('/api/auth', authRouter);
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
@@ -33,3 +60,7 @@ server.use((err, req, res, next) => { // eslint-disable-line
 });
 
 module.exports = server;
+
+
+
+
